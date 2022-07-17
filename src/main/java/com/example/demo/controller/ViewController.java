2ed89958dc37a053e6bcf02b5e9c5324e6ff2d4d
Pownyfals <pownyfals@gmail.com>
2022-07-17 17:03:34 +0700

    add card for books

--- src/main/java/com/example/demo/controller/ViewController.java
+++ src/main/java/com/example/demo/controller/ViewController.java
@@ -2,7 +2,9 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -12,4 +14,11 @@ public class ViewController {
         return "index";
     }
 
+    @GetMapping("https://www.googleapis.com/books/v1/volumes?q={judul}")
+    public String search(@PathVariable String judul)  {
+        return "index";
+    }
+
 }
+
+
